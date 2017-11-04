d5ac9db5f6f5314bef07051b6325edf8ec9dbcc0
Andrey Davidovich <cri2net@gmail.com>
2017-11-04 15:38:33 +0200

    Добавил функцию getAllHeaders, которая пытается определить заголовки запроса

--- src/Core/Http.php
+++ src/Core/Http.php
@@ -144,4 +144,46 @@ class Http
 
         return file_get_contents($url, false, $context);
     }
+
+    /**
+     * Метод парсит переменную $_SERVER и генерирует массив с HTTP Заголовками запроса
+     * Как аналог \getallheaders(), но она доступна только при работе php из-под apache
+     * @link https://github.com/ralouphie/getallheaders/ Оригинальный код функции
+     * 
+     * @return array http headers
+     */
+    public static function getAllHeaders()
+    {
+        $headers = [];
+        $copy_server = [
+            'CONTENT_TYPE'   => 'Content-Type',
+            'CONTENT_LENGTH' => 'Content-Length',
+            'CONTENT_MD5'    => 'Content-Md5',
+        ];
+        
+        foreach ($_SERVER as $key => $value) {
+            if (substr($key, 0, 5) === 'HTTP_') {
+                $key = substr($key, 5);
+                if (!isset($copy_server[$key]) || !isset($_SERVER[$key])) {
+                    $key = str_replace(' ', '-', ucwords(strtolower(str_replace('_', ' ', $key))));
+                    $headers[$key] = $value;
+                }
+            } elseif (isset($copy_server[$key])) {
+                $headers[$copy_server[$key]] = $value;
+            }
+        }
+
+        if (!isset($headers['Authorization'])) {
+            if (isset($_SERVER['REDIRECT_HTTP_AUTHORIZATION'])) {
+                $headers['Authorization'] = $_SERVER['REDIRECT_HTTP_AUTHORIZATION'];
+            } elseif (isset($_SERVER['PHP_AUTH_USER'])) {
+                $basic_pass = isset($_SERVER['PHP_AUTH_PW']) ? $_SERVER['PHP_AUTH_PW'] : '';
+                $headers['Authorization'] = 'Basic ' . base64_encode($_SERVER['PHP_AUTH_USER'] . ':' . $basic_pass);
+            } elseif (isset($_SERVER['PHP_AUTH_DIGEST'])) {
+                $headers['Authorization'] = $_SERVER['PHP_AUTH_DIGEST'];
+            }
+        }
+        
+        return $headers;
+    }
 }
